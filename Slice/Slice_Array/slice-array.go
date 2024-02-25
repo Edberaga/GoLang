8608a8ea1bb62dd080392064165abc731d03b69e
Edberaga <edbetjonnathan@gmail.com>
2024-02-25 17:03:20 +0700

    Learn the difference between slice and array

---
+++ Slice/Slice_Array/slice-array.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Array is based on value, while Slice is based on references
+	
+	//We will try array Pass by Value
+	numbers := [5]int{2, 1, 4, 8, 7}
+	anotherNum := numbers
+	fmt.Println("Array Values: ", numbers, "\nDuplicate Values: ", anotherNum)
+
+	anotherNum[3] = 16
+	fmt.Println("Array Values: ", numbers, "\nDuplicate Values: ", anotherNum)
+	fmt.Println("========================================================================")
+
+	//Now try to do it without set the length, which going to be Pass by reference
+	prices := []int{2, 1, 4, 8, 7}
+	anotherPrices := prices
+	fmt.Println("Array Values: ", prices, "\nDuplicate Values: ", anotherPrices)
+
+	anotherPrices[3] = 16
+	fmt.Println("Array Values: ", prices, "\nDuplicate Values: ", anotherPrices)
+	fmt.Println("========================================================================")
+
+	//Now lets try Slice
+	ages := [4]string{"Warrior", "Mage", "Rogue", "Cleric"}
+	anotherAge := ages[0:len(ages)] //Here's how Slice duplicate an array, can write ages[0:4] but this better
+	fmt.Println("Array Values: ", ages)
+	fmt.Println("Slice Values: ", anotherAge)
+
+	anotherAge[2] = "Archer" //Because Slice is based on reference, if its changed it will also change the one it duplicates
+	fmt.Println("Array Values: ", ages)
+	fmt.Println("Slice Values: ", anotherAge)
+}
