07e901acf0127e4cfd637031336e65454f4ddc38
Edberaga <edbetjonnathan@gmail.com>
2024-02-25 17:03:40 +0700

    array exercises

---
+++ Array/Exercise/array.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+)
+ 
+func main() {
+    var elems int
+    
+    fmt.Print("Number of elements? ")
+    fmt.Scan(&elems)
+    
+    var array = make([]int,elems)
+    
+    for i := 0; i < elems; i++ {
+        fmt.Printf("%d . Number? ", i+1)
+        fmt.Scan(&array[i])
+    }
+
+    for i := range array {
+        if(array[i] % 2 == 0) {
+            fmt.Println(array[i])
+        }
+    }
+}
